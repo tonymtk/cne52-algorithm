44366193c398db1197082922831e07f96b5281fb
Vinícius Antonioli <viniciusantonioli.dev@gmail.com>
2020-11-18 10:43:29 -0300

    Update cne52.py

--- cne52.py
+++ cne52.py
@@ -1,11 +1,11 @@
 """
 © Copyright 2001-2018, Python Software Foundation.
-Algorithm created by Vinícius Antonioli(tonymtk)
+Algorithm created by Vinícius Antonioli
 License: GNU Public License(https://www.gnu.org/licenses/gpl.txt)
 """
 print('----------------------------------------------------------------')
 titulo = ('- Algoritmo CNE52')
-repositorio = ('https://github.com/tonymtk/cne52-algorithm')
+repositorio = ('https://github.com/viniciusantonioli/cne52-algorithm')
 print(titulo)
 presenca = float(input('Digite a nota de presença do aluno: '))
 atividades = float(input('Digite a nota de atividades do aluno: '))
@@ -16,4 +16,3 @@ resultado = str(('A nota final do aluno é {:.3}'.format(divisao)))
 print(resultado)
 print('\n----------------------------------------------------------------')
 print('Repositório oficial: {}'.format(repositorio))
-print('Algorithm created by Vinícius Antonioli(tonymtk)\nLicense: GNU Public License')
